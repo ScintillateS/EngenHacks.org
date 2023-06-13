4172657ecd729a0ef0fe5e0ca72b83e0a9ea6db1
MusaAqeel <77996753+MusaAqeel@users.noreply.github.com>
2023-06-12 22:26:54 -0400

    possible team link fix

--- components/sections/Team.jsx
+++ components/sections/Team.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import Image from 'next/image';
 import { styled, css } from '~stitches';
 import teamData from './teamData';
-
-import { Header } from '../atoms';
 
 const Wrapper = styled('section', {
   minHeight: '100vh',
@@ -12,19 +9,18 @@ const Wrapper = styled('section', {
   scrollMarginTop: '12vh',
 });
 
-const TeamGrid = styled('div', css({
+const TeamGrid = styled('div', {
   display: 'grid',
   gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))',
   gap: '2.5rem',
   maxWidth: '1142px',
-  // background: 'linear-gradient(101.99deg, rgba(249, 249, 249, 0.5) 5.49%, rgba(249, 249, 249, 0.3) 100%)',
   boxSizing: 'border-box',
   boxShadow: '0px 4px 24px -1px rgba(0, 0, 0, 0.25)',
   backdropFilter: 'blur(20px)',
   margin: '0 auto',
   borderRadius: '10px',
   padding: '50px',
-}));
+});
 
 const TeamMemberContainer = styled('div', {
   textAlign: 'center',
@@ -47,50 +43,42 @@ const TeamMemberImage = styled('div', {
   },
 });
 
-const LearnMoreButton = styled('a', {
-  animation: 'pulse 1s infinite',
+const LearnMoreLink = styled('a', {
   fontSize: '22px',
   fontWeight: 'bolder',
-  fontFamily: '"NTR"',
+  fontFamily: '$filsonPro',
   padding: '10px 20px',
-  textDecoration: 'none !important',
+  textDecoration: 'none',
   cursor: 'pointer',
-  border: '3.50px solid var(--blue)',
+  border: '3.5px solid $blue',
   borderRadius: '14px',
   transition: 'border-color 0.2s ease-out, color 0.2s ease-out',
-  color: 'var(--slate)',
+  color: '$slate',
   marginTop: '10px',
   '& img': {
     height: '18px',
   },
   '&:hover': {
-    color: 'var(--blue) !important',
-    textDecoration: 'none !important',
+    color: '$blue',
+    textDecoration: 'none',
     transition: 'color 0.2s ease-out, text-decoration 0.2s ease-out',
   },
 });
-
-
-
 
 const TeamMember = ({ name, role, img, highSchoolYear, experience, link }) => {
   return (
     <TeamMemberContainer>
       <TeamMemberImage>
-        <Image src={img} alt={name} width={200} height={200} />
+        <img src={img} alt={name} width={200} height={200} />
       </TeamMemberImage>
       <h3>{name}</h3>
       <p>{role}</p>
-      <LearnMoreButton href={link} target="_blank" rel="noopener noreferrer">
-
-        <a href={link} target="_blank" rel="noopener noreferrer">
-          {link}
-        </a>
-      </LearnMoreButton>
+      <LearnMoreLink href={link} target="_blank" rel="noopener noreferrer">
+        {link}
+      </LearnMoreLink>
     </TeamMemberContainer>
   );
 };
-
 
 const Team = () => {
   return (
@@ -105,7 +93,7 @@ const Team = () => {
             img={member.image}
             highSchoolYear={member.highSchoolYear}
             experience={member.experience}
-            link={member.link} // Pass the "link" property to the TeamMember component
+            link={member.link}
           />
         ))}
       </TeamGrid>
